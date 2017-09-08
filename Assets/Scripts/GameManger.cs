2af1c6c148ccdff084830f57a6599e106549187c
DynamicDonut <bwilldonut@gmail.com>
2017-09-07 23:48:52 -0400

    Questions + Beginning TextMeshPro work
    Sourcing questions from Open TriviaDB via JSON.

---
+++ Assets/Scripts/GameManger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using LitJson;
+
+public class GameManger : MonoBehaviour {
+    private JsonData questionData;
+    public Transform myUI;
+    public int currQuestion;
+    bool foundQuestions;
+    WWW myWWW;
+
+    // Use this for initialization
+    void Start() {
+        myWWW = new WWW("https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple&encode=url3986");
+        foundQuestions = false;
+        currQuestion = Random.Range(0, 9);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (myWWW.isDone && !foundQuestions) {
+            questionData = JsonMapper.ToObject(myWWW.text);
+            for (int i = 0; i < questionData["results"].Count; i++) {
+                Debug.Log(WWW.UnEscapeURL(questionData["results"][i]["question"].ToString()));
+            }
+            foundQuestions = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Question.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+[System.Serializable]
+public class Question {
+    public string question;
+    public List<string> allAnswers;
+    public string correctAnswer;
+}
