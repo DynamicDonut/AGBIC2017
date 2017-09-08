1816e2c59d59891ec439b4e5672c1d118e3c32dc
DynamicDonut <bwilldonut@gmail.com>
2017-09-08 03:46:39 -0400

    Random Question Generation + more
    Added UI Buttons, buttons will turn green if correct, red if wrong. If Wrong, the correct answer will be highlighted.
    
    9Slice Sprite test as well, to test out UI.

---
+++ Assets/Scripts/AnswerCheck.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AnswerCheck : MonoBehaviour {
+	public GameManger myGM;
+	public Color defaultColor;
+	public bool amIRight;
+
+	void Start(){
+		myGM = GameObject.Find ("GameManager").GetComponent<GameManger>();
+		amIRight = false;
+		defaultColor = GetComponent<Image> ().color;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void CheckAnswer(){
+		if (amIRight) {
+			GetComponent<Image>().color = Color.green;
+		} else {
+			GetComponent<Image>().color = Color.red;
+		}
+		myGM.questionAnswered = true;
+	}
+
+	public void CheckValidAnswer(){
+		if (transform.GetChild (0).GetComponent<TextMeshProUGUI> ().text == WWW.UnEscapeURL (myGM.questionData ["results"] [myGM.currQuestion] ["correct_answer"].ToString ())) {
+			amIRight = true;
+		} 
+	}
+}
--- Assets/Scripts/GameManger.cs
+++ Assets/Scripts/GameManger.cs
@@ -1,29 +1,92 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
 using LitJson;
+using TMPro;
 
 public class GameManger : MonoBehaviour {
-    private JsonData questionData;
+    public JsonData questionData;
     public Transform myUI;
-    public int currQuestion;
-    bool foundQuestions;
+	public List<int> questionList;
+	public int currQuestion, numofQuestions;
+	public List<string> currAnswers;
+    public bool foundQuestions, foundAnswers, questionAnswered;
     WWW myWWW;
 
     // Use this for initialization
     void Start() {
-        myWWW = new WWW("https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple&encode=url3986");
-        foundQuestions = false;
-        currQuestion = Random.Range(0, 9);
+		myWWW = new WWW("https://opentdb.com/api.php?amount=" + numofQuestions + "&category=15&difficulty=medium&type=multiple&encode=url3986");
+        foundQuestions = foundAnswers = false;
+		currQuestion = Random.Range(0, numofQuestions);
+		questionList.Add (currQuestion);
     }
 
     // Update is called once per frame
     void Update() {
         if (myWWW.isDone && !foundQuestions) {
             questionData = JsonMapper.ToObject(myWWW.text);
-            for (int i = 0; i < questionData["results"].Count; i++) {
-                Debug.Log(WWW.UnEscapeURL(questionData["results"][i]["question"].ToString()));
-            }
+//            for (int i = 0; i < questionData["results"].Count; i++) {
+//                Debug.Log(WWW.UnEscapeURL(questionData["results"][i]["question"].ToString()));
+//            }
             foundQuestions = true;
+			StartCoroutine (GenerateNewQuestion(0f));
         }
+
+		if (questionAnswered){
+			HighlightRightAnswer ();
+			currQuestion = Random.Range (0, numofQuestions);
+			for (int i = 0; i < questionList.Count; i++) {
+				if (currQuestion == questionList [i]) {
+					currQuestion = Random.Range (0, numofQuestions);
+				}
+			}
+			StartCoroutine (GenerateNewQuestion(2.0f));
+			questionAnswered = false;
+		}
+
+		//Debug.Log (questionData ["results"] [currQuestion] ["correct_answer"].ToString ());
     }
+
+	IEnumerator GenerateNewQuestion(float waitTime){
+		yield return new WaitForSeconds (waitTime);
+		myUI.Find("Question").GetChild(0).GetComponent<TextMeshProUGUI> ().text = WWW.UnEscapeURL (questionData ["results"] [currQuestion] ["question"].ToString ());
+		foundAnswers = false;
+		RandomizeAnswers ();
+		for (int i = 0; i < currAnswers.Count; i++) {
+			myUI.Find ("Answer" + (i + 1)).GetChild(0).GetComponent<TextMeshProUGUI> ().text = currAnswers [i];
+			myUI.Find ("Answer" + (i + 1)).GetComponent<AnswerCheck> ().amIRight = false;
+			myUI.Find ("Answer" + (i + 1)).GetComponent<AnswerCheck> ().CheckValidAnswer ();
+			myUI.Find ("Answer" + (i + 1)).GetComponent<Image> ().color = myUI.Find ("Answer" + (i + 1)).GetComponent<AnswerCheck> ().defaultColor;
+
+		}
+	}
+		
+	void RandomizeAnswers(){
+		currAnswers.Clear ();
+		if (!foundAnswers) {
+			currAnswers.Add (WWW.UnEscapeURL (questionData ["results"] [currQuestion] ["correct_answer"].ToString ()));
+			for (int i = 0; i < 3; i++) {
+				currAnswers.Add (WWW.UnEscapeURL (questionData ["results"] [currQuestion] ["incorrect_answers"] [i].ToString ()));
+			}
+
+			System.Random newRand = new System.Random ();
+			string temp = "temp";
+			for (int i = 0; i < currAnswers.Count; i++) {
+				int r = i + (int)(newRand.NextDouble() * (currAnswers.Count - i));
+				temp = currAnswers[r];
+				currAnswers[r] = currAnswers[i];
+				currAnswers[i] = temp;
+			}
+			foundAnswers = true;
+		}
+	}
+
+	void HighlightRightAnswer(){
+		for (int i = 0; i < 4; i++) {
+			if (myUI.GetChild (i + 1).GetComponent<AnswerCheck> ().amIRight == true) {
+				myUI.GetChild (i + 1).GetComponent<Image> ().color = Color.green;
+			}
+		}
+	}
 }
